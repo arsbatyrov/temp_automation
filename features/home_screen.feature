e93c5d8c45d2d61799f39c1fd83051f282dd73b1
comolder <comolder@gmail.com>
2017-12-06 02:10:27 +0300

    Step 2: First test

---
+++ features/home_screen.feature
@@ -0,0 +1,6 @@
+Feature: I want to convert units
+
+  Scenario: When I tap on Menu icon, I should see menu
+    When I land on Area screen
+    Then I tap menu icon
+    And I see app menu
---
+++ features/step_definitions/home_screen_steps.rb
@@ -0,0 +1,11 @@
+When(/^I land on Area screen$/) do
+  puts("User lands on Area screen")
+end
+
+Then(/^I tap menu icon$/) do
+  puts("User taps on menu icon")
+end
+
+And(/^I see app menu$/) do
+  puts("User sees app menu")
+end
